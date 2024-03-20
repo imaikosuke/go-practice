5b24d0ae41ff6f59a8ae2e293c316c8bc9216a9f
imaikosuke <imaikosuke2@gmail.com>
2024-03-20 23:52:52 +0900

    HTTPサーバの基礎

---
+++ practice1.go
@@ -0,0 +1,17 @@
+package main  // パッケージ名をmainにすることで、実行可能なプログラムを作成することができる
+
+import (
+	"fmt"       // 標準入出力に使用
+	"net/http"  // HTTPサーバーを作成するために使用
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { // ルートパスにアクセスがあった場合の処理
+		fmt.Fprintf(w, "こんにちは、Go言語の世界へ!")                 // レスポンスを返す
+	})
+
+	fmt.Println("http://localhost:8080 でサーバーを起動します。")   // サーバーを起動する旨を表示
+	if err := http.ListenAndServe(":8080", nil); err != nil {  	// サーバーを起動
+		panic(err)                                                // エラーが発生した場合はエラー内容を表示
+	}
+}
